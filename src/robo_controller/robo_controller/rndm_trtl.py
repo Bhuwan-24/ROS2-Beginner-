88f277e37c6e071a16c83c5b6631ebeba03bace3
Bhuwan-24 <bhuwanadhikari775@gmail.com>
2026-05-15 19:49:30 +0530

    added logic to rotate without stucking

--- src/robo_controller/robo_controller/rndm_trtl.py
+++ src/robo_controller/robo_controller/rndm_trtl.py
@@ -7,19 +7,23 @@ from rclpy.node import Node
 class rndm_mvmt(Node):
     def __init__(self):
         super().__init__("random_movement")
+        
         self.vel=self.create_publisher(Twist,"/turtle1/cmd_vel",10)
         self.pos=self.create_subscription(Pose,"/turtle1/pose",self.rndm_mvmt,10)
+        self.rotate=0.0
 
 
     def rndm_mvmt(self,p=Pose):
         v=Twist()
         
-        if p.x>9 or p.x<1 or p.y>9 or p.y<1:
-            rotate=random.uniform(-1,1)
+        if p.x>9 or p.x<2 or p.y>9 or p.y<2:
+            if self.rotate==0.0:
+                self.rotate=random.uniform(-3,3)
             v.linear.x=1.0
-            v.angular.z=rotate
+            v.angular.z=self.rotate
 
         else:
+            self.rotate=0.0
             v.linear.x=1.0
             v.angular.z=0.0
         self.vel.publish(v)
