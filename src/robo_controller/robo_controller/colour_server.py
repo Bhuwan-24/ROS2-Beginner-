da787d7ef4cb9f5a5e53e25708ef0b83cd74740a
Bhuwan-24 <bhuwanadhikari775@gmail.com>
2026-05-23 22:08:00 +0530

    service to change the turtle pen colour

---
+++ src/robo_controller/robo_controller/colour_server.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+import rclpy
+from rclpy.node import Node
+from turtlesim.srv import SetPen
+
+class set_color(Node):
+    def __init__(self):
+        super().__init__("change_colour")
+        self.srv=self.create_service(SetPen,"change_pen_color",self.colour)
+
+        self.get_logger().info("service started..........")
+
+    def colour(self,request,response):
+        
+        self.get_logger().info(f"r : {request.r}, g : {request.g}, b : {request.b}")
+
+        return response
+    
+    def main(args=None):
+        rclpy.init(args=args)
+        node=set_color()
+        rclpy.spin(node)
+        rclpy.shutdown()
