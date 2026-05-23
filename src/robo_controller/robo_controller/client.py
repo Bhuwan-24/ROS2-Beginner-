61bdf1c31a71b5c05cbec04e6404530a58c66447
Bhuwan-24 <bhuwanadhikari775@gmail.com>
2026-05-23 20:44:05 +0530

    client for service

---
+++ src/robo_controller/robo_controller/client.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+import rclpy
+from rclpy.node import Node
+from example_interfaces.srv import AddTwoInts
+
+class add_client(Node):
+    def __init__(self):
+        super().__init__("add_client")
+        self.client=self.create_client(AddTwoInts,"add_here")
+
+        while not self.client.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info("waiting for server to be free...........")
+        
+        self.request=AddTwoInts.Request()
+
+    def request(self,a,b):
+        self.request.a=a
+        self.request.b=b
+        future =self.client.call_async(self.request)
+        rclpy.spin_until_future_complete(self,future)
+        return future.result()
+
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    client=add_client()
+    response=client.request(5,2)
+    client.get_logger().info(f"Response : {response.sum}")
+    rclpy.shutdown()
+    
+    
