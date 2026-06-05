a8fb036ae61c9f7c61b358e26d9402d0906f5905
Bhuwan-24 <bhuwanadhikari775@gmail.com>
2026-06-05 22:12:06 +0530

    everything alright

--- src/robo_controller/robo_controller/launch/master_follower.launch.py
+++ src/robo_controller/robo_controller/launch/master_follower.launch.py
@@ -4,16 +4,22 @@ from launch_ros.actions import Node
 def generate_launch_description():
     ld=LaunchDescription()
 
+    env=Node(
+        package="turtlesim",
+        executable="turtlesim_node"
+
+    )
+
     master=Node(
         package="robo_controller",
-        executable="random_rotate"
+        executable="smart_turtle"
     )
 
     follower=Node(
         package="robo_controller",
         executable="path_follower"
     )
-
+    ld.add_action(env)
     ld.add_action(master)
     ld.add_action(follower)
     return ld
